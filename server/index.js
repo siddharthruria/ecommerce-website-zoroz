fa19e75ffbfe6fae81529fd984bfc57ce0b669d5
Siddharth Ruria <siddharthruria10@gmail.com>
2024-11-05 12:33:55 +0530

    setup react client side

--- server/index.js
+++ server/index.js
@@ -2,10 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin: "https://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
 app.use(cookieParser());
+
 connectDB();
 
 const userRoutes = require("./route/user");
